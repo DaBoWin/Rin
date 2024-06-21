145c784ec8a0f13acd5778e314bbe5cdb76bface
DaBoWin <165022983+DaBoWin@users.noreply.github.com>
2024-06-21 15:52:36 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -32,10 +32,10 @@ function Footer() {
             <div className="flex flex-col mb-8 space-y-2 justify-center items-center h-16 t-primary ani-show">
                 <p className='text-sm text-neutral-500 font-normal link-line'>
                     <span>
-                        © 2024 Powered by <a className='hover:underline' href="https://github.com/OXeu/Rin" target="_blank">Rin</a>
+                        © 2024 Powered by <a className='hover:underline' href="https://github.com/DaBoWin" target="_blank">DaBo</a>
                     </span>
                     {process.env.RSS_ENABLE === "true" && <>
-                        <Spliter />
+                        <Spliter />{/*  */}
                         <Popup trigger={
                             <button className="hover:underline" type="button">
                                 RSS
